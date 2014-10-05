86552205ce6ea173fe1b0a33429d0fc58d38c25a
paulineml <paulineml@hotmail.com>
2014-10-05 13:39:43 +0200

    Deuxieme partie fonction parse

--- src/projet_ppm1.c
+++ src/projet_ppm1.c
@@ -10,6 +10,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	unsigned char red, green, blue;
+} PPMPixel;
+
+typedef struct {
+	int x, y;
+	PPMPixel *data;
+} PPMImage;
+
+#define RGB_COMPONENT_COLOR 255
+
 FILE *readPpmFile(void);
 int parsePpmFile(FILE *fp);
 FILE *seuillagePpmFile(void);
@@ -55,7 +66,8 @@ FILE *readPpmFile(void) {
 			filename);
 	fp = fopen(filename, "rb");
 	if (!fp) {
-		fprintf(stderr, "%s-%03d: Unable to open file '%s'\n", __FILE__, __LINE__, filename);
+		fprintf(stderr, "%s-%03d: Unable to open file '%s'\n", __FILE__,
+		__LINE__, filename);
 		exit(1);
 	}
 	printf("%s-%03d:   readPpmFile end\n", __FILE__, __LINE__);
@@ -64,32 +76,38 @@ FILE *readPpmFile(void) {
 }
 int parsePpmFile(FILE *fp) {
 	char buff[16];
+	int c, rgb_comp_color;
+	PPMImage *img;
 
 	printf("%s-%03d:   parsePpmFile start\n", __FILE__, __LINE__);
-	//read image format
+
+	printf("%s-%03d:   read image format\n", __FILE__, __LINE__);
 	if (!fgets(buff, sizeof(buff), fp)) {
-		fprintf(stderr, "%s-%03d:   problem when reading file\n", __FILE__, __LINE__);
+		fprintf(stderr, "%s-%03d:   problem when reading file\n", __FILE__,
+		__LINE__);
 		exit(1);
 	}
 
-	//check the image format
+	printf("%s-%03d:   check the image format\n", __FILE__, __LINE__);
 	if (buff[0] != 'P' || buff[1] != '3') {
 		printf("%s-%03d:   %c %c\n", __FILE__, __LINE__, buff[0], buff[1]);
-		fprintf(stderr, "%s-%03d:   Invalid image format (must be 'P3')\n", __FILE__, __LINE__);
+		fprintf(stderr, "%s-%03d:   Invalid image format (must be 'P3')\n",
+		__FILE__, __LINE__);
 		exit(1);
-	}
-	else {
+	} else {
 		printf("%s-%03d:   this is a P3 file\n", __FILE__, __LINE__);
 	}
 
-	//alloc memory form image
+	printf("%s-%03d:   alloc memory form image\n", __FILE__, __LINE__);
+
 	img = (PPMImage *) malloc(sizeof(PPMImage));
 	if (!img) {
-		fprintf(stderr, "Unable to allocate memory\n");
+		fprintf(stderr, "%s-%03d:   Unable to allocate memory\n", __FILE__,
+		__LINE__);
 		exit(1);
 	}
 
-	//check for comments
+	printf("%s-%03d:   check for comments\n", __FILE__, __LINE__);
 	c = getc(fp);
 	while (c == '#') {
 		while (getc(fp) != '\n')
@@ -98,40 +116,47 @@ int parsePpmFile(FILE *fp) {
 	}
 
 	ungetc(c, fp);
-	/*read image size information
+	printf("%s-%03d:   read image size information\n", __FILE__, __LINE__);
 	if (fscanf(fp, "%d %d", &img->x, &img->y) != 2) {
-		fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
+		fprintf(stderr, "%s-%03d:   Invalid image size (error loading)\n",
+				__FILE__, __LINE__);
 		exit(1);
+	} else {
+		printf("%s-%03d:   col: %d, row: %d\n", __FILE__, __LINE__, img->x,
+				img->y);
 	}
 
-	//read rgb component
+	printf("%s-%03d:   read rgb component\n", __FILE__, __LINE__);
 	if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
-		fprintf(stderr, "Invalid rgb component (error loading '%s')\n",
-				filename);
+		fprintf(stderr, "%s-%03d:   Invalid rgb component (error loading')\n",
+				__FILE__,
+				__LINE__);
 		exit(1);
 	}
 
-	//check rgb component depth
+	printf("%s-%03d:   check rgb component depth\n", __FILE__, __LINE__);
 	if (rgb_comp_color != RGB_COMPONENT_COLOR) {
-		fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
+		fprintf(stderr, "%s-%03d:   file does not have 8-bits components\n",
+				__FILE__, __LINE__);
 		exit(1);
 	}
 
-	while (fgetc(fp) != '\n')
-		;
-	//memory allocation for pixel data
-	img->data = (PPMPixel*) malloc(img->x * img->y * sizeof(PPMPixel));
+	/*while (fgetc(fp) != '\n')
+	 ;
+	 //memory allocation for pixel data
+	 img->data = (PPMPixel*) malloc(img->x * img->y * sizeof(PPMPixel));
 
-	if (!img) {
-		fprintf(stderr, "Unable to allocate memory\n");
-		exit(1);
-	}
+	 if (!img) {
+	 fprintf(stderr, "Unable to allocate memory\n");
+	 exit(1);
+	 }
 
-	//read pixel data from file
-	if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
-		fprintf(stderr, "Error loading image '%s'\n", filename);
-		exit(1);
-	}*/
+	 //read pixel data from file
+	 if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
+	 fprintf(stderr, "Error loading image '%s'\n", filename);
+	 exit(1);
+	 }
+	 */
 
 	fclose(fp);
 	printf("%s-%03d:   parsepmFile end\n", __FILE__, __LINE__);
