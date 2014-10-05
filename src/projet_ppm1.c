8a7cb052ca94548b61a97e695c04959360e812e4
paulineml <paulineml@hotmail.com>
2014-10-05 11:07:52 +0200

    read a ppm file
    (cygwin specific)

--- src/projet_ppm1.c
+++ src/projet_ppm1.c
@@ -7,24 +7,29 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 FILE *readPpmFile(void);
-FILE *parsePpmFile(void);
+int parsePpmFile(FILE *fp);
 FILE *seuillagePpmFile(void);
 FILE *filtragePpmFile(void);
 FILE *outputPpmFile(void);
 
+// http://stackoverflow.com/questions/2693631/read-ppm-file-and-store-it-in-an-array-coded-with-c
 int main(void) {
 	FILE *ppmFileIn;
 	FILE *ppmFileOut;
+
+	// http://www.eclipse.org/forums/index.php/t/168233/
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
 
 	printf("%s-%03d: !!!Hello World!!!\n", __FILE__, __LINE__);
 	printf("%s-%03d: read ppm file\n", __FILE__, __LINE__);
 	ppmFileIn = readPpmFile();
 	printf("%s-%03d: parse ppm file\n", __FILE__, __LINE__);
+	parsePpmFile(ppmFileIn);
 	//tabPixels = parsePpmFile(ppmFileIn);
 	printf("%s-%03d: seuillage\n", __FILE__, __LINE__);
 	//ppmFile = seuillagePpmFile();
@@ -46,17 +51,23 @@ FILE *readPpmFile(void) {
 	printf("%s-%03d:   readPpmFile start\n", __FILE__, __LINE__);
 	printf("%s-%03d:   Nom du fichier d\'entrée?\n", __FILE__, __LINE__);
 	scanf("%s", &filename);
-	printf("%s-%03d:   Nom du fichier d\'entrée: %s\n", __FILE__, __LINE__, filename);
+	printf("%s-%03d:   Nom du fichier d\'entrée: %s\n", __FILE__, __LINE__,
+			filename);
 	fp = fopen(filename, "rb");
+	if (!fp) {
+		fprintf(stderr, "Unable to open file '%s'\n", filename);
+		exit(1);
+	}
 	printf("%s-%03d:   readPpmFile end\n", __FILE__, __LINE__);
 
 	return fp;
 }
-FILE *parsePpmFile(void) {
-	FILE *fp = 0;
+int parsePpmFile(FILE *fp) {
 	printf("%s-%03d:   parsePpmFile start\n", __FILE__, __LINE__);
+
+	fclose(fp);
 	printf("%s-%03d:   parsepmFile end\n", __FILE__, __LINE__);
-	return fp;
+	return 0;
 }
 FILE *seuillagePpmFile(void) {
 	FILE *fp = 0;
